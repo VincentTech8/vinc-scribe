59103d41ff7d3e97a20b319150ed15e31e44a9e1
Vincent Vincent <toxicgeek8@gmail.com>
2024-10-11 16:55:28 +1100

    Modify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header className='flex items-center justify-between gap-4 p-4'>
-            <a href="/"><h1 className='font-bold text-xl sm:text-2xl bg-clip-text text-transparent hover:opacity-70 bg-black'>Vinc<span className='bg-clip-text text-transparent bg-blue-500 bold'>Scribe.</span></h1></a>
+            <a href="/"><h1 className='font-bold text-xl sm:text-2xl bg-clip-text text-transparent hover:opacity-70 bg-black'>V<span className='bg-clip-text text-transparent bg-blue-500 bold'>Linguist.</span></h1></a>
             <div className='gap-4 flex items-center '>
                 <a href="/" className='flex items-center gap-2 px-3 py-2 border border-blue-200 rounded-lg text-blue-500 shadow-md hover:text-opacity-70 hover:bg-slate-100'>
                     <p>New</p>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Homepage = () => {
     return (
         <main className='flex flex-auto flex-col gap-3 sm:gap-4 text-center justify-center p-4 pb-20'>
-            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Vinc<span className='text-blue-500 bold'>Scribe</span></h1>
+            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>V<span className='text-blue-500 bold'>Linguist</span></h1>
             <h3 className='font-medium md:text-lg'>Record <span className='text-blue-500'>&rarr;</span> Transcribe <span className='text-blue-500'>&rarr;</span> Translate</h3>
             <button className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
                 <p className='text-blue-500'>Record</p>
@@ -12,7 +12,7 @@ const Homepage = () => {
                 </div>
             </button>
             <p className='text-base'>Or <label className='text-blue-500 cursor-pointer hover:text-blue-600 duration-200'>upload <input className='hidden' type='file' accept='.mp3,.wave' /></label> a mp3 file</p>
-            <p className='italic text-slate-400'>Free now free forever</p>
+            <p className='italic text-slate-400'>Free now free forever!</p>
         </main>
     )
 };
